2ac56333aacece5a794b786f47761103b2eda063
izansl <125253405+izansl@users.noreply.github.com>
2025-01-23 20:49:37 +0100

    Fix Lobby

--- FIKI/Assets/Fiki/Scripts/General Fiki Scripts/LevelSelector.cs
+++ FIKI/Assets/Fiki/Scripts/General Fiki Scripts/LevelSelector.cs
@@ -31,7 +31,7 @@ public class LevelSelector : MonoBehaviour
 
     public void Minigame4()
     {
-        //LevelTransitionController.instance.StartTransition(11, 0.5f);
+        LevelTransitionController.instance.StartTransition(15, 0.5f);
     }
 
     public void Minigame5()
